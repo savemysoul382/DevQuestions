8c3310c5c271923ce25929e12a25395c2f82ae36
savemysoul382 <serjke88@gmail.com>
2026-01-13 18:36:52 +0300

    Реализация паттерна Command-Handler для вопросов/ответов
    Введены интерфейсы ICommand и ICommandHandler, реализованы команды и обработчики для создания вопросов и добавления ответов. Валидация DTO перенесена в обработчики команд. DI обработчиков автоматизирован с помощью Scrutor. Контроллер теперь использует ICommandHandler вместо прямого обращения к сервису. Проведен рефакторинг QuestionsService и структуры проекта для поддержки CQRS.

---
+++ DevQuestions/src/DevQuestions.Application/Questions/Features/CreateQuestion/CreateQuestionHandler.cs
@@ -0,0 +1,80 @@
+﻿// DevQuestions.Application
+
+using CSharpFunctionalExtensions;
+using DevQuestions.Application.Abstractions;
+using DevQuestions.Application.Extensions;
+using DevQuestions.Application.Questions.Fails;
+using DevQuestions.Contracts.Questions;
+using DevQuestions.Domain.Questions;
+using FluentValidation;
+using FluentValidation.Results;
+using Microsoft.Extensions.Logging;
+using Shared;
+
+namespace DevQuestions.Application.Questions.CreateQuestion;
+
+public class CreateQuestionHandler : ICommandHandler<Guid, CreateQuestionCommand>
+{
+    private readonly IQuestionsRepository _questionsRepository;
+    private readonly IValidator<CreateQuestionDto> _createQuestionDtoValidator;
+    private readonly ILogger<QuestionsService> _logger;
+
+    public CreateQuestionHandler(
+        IQuestionsRepository questionsRepository,
+        IValidator<CreateQuestionDto> createQuestionDtoValidator,
+        ILogger<QuestionsService> logger)
+    {
+        _questionsRepository = questionsRepository;
+        _logger = logger;
+        _createQuestionDtoValidator = createQuestionDtoValidator;
+    }
+
+    public async Task<Result<Guid, Failure>> HandleAsync(CreateQuestionCommand command, CancellationToken cancellationToken)
+    {
+        // проверка валидности
+        ValidationResult validationResult = await _createQuestionDtoValidator.ValidateAsync(instance: command.QuestionDto, cancellation: cancellationToken);
+        if (!validationResult.IsValid)
+        {
+            return validationResult.ToErrors();
+        }
+
+        var calculator = new QuestionsService.QuestionCalculator();
+
+        Result<int, Failure> calculateResult = calculator.Calculate();
+
+        if (calculateResult.IsFailure)
+        {
+            return calculateResult.Error;
+        }
+
+        int calculateResultValue = calculateResult.Value;
+
+        // валидация бизнес логики
+        int openUserQuestionsCount = await _questionsRepository
+            .GetOpenUserQuestionsAsync(userId: command.QuestionDto.UserId, cancellationToken: CancellationToken.None);
+
+        if (openUserQuestionsCount >= 3)
+        {
+            return Errors.Question.ToManyQuestions.ToFailure();
+        }
+
+        // создание сущности Question
+        var questionId = Guid.NewGuid();
+
+        var question = new Question(
+            id: questionId,
+            title: command.QuestionDto.Title,
+            text: command.QuestionDto.Text,
+            userId: command.QuestionDto.UserId,
+            null,
+            command.QuestionDto.TagIds.ToList());
+
+        // сохранение в БД
+        await _questionsRepository.AddAsync(question: question, cancellationToken: CancellationToken.None);
+
+        // логирование об успешном или неуспешном сохранении
+        _logger.LogInformation("Question with ID {QuestionId} created successfully.", questionId);
+
+        return questionId;
+    }
+}
---
+++ DevQuestions/src/DevQuestions.Application/Questions/Features/SelectSolution/SelectSolutionCommand.cs
@@ -0,0 +1,17 @@
+﻿// DevQuestions.Application
+
+using CSharpFunctionalExtensions;
+using DevQuestions.Application.Abstractions;
+using Shared;
+
+namespace DevQuestions.Application.Questions.SelectSolution;
+
+// public record SelectSolutionCommand() : ICommand;
+
+// public class SelectSolutionHandler : ICommandHandler<Guid, SelectSolutionCommand>
+// {
+//    public Task<Result<Guid, Failure>> HandleAsync(SelectSolutionCommand command, CancellationToken cancellationToken)
+//    {
+//        throw new NotImplementedException();
+//    }
+// }
